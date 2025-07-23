45bd47a360c2fa54db95500c176b906f9f5f4d4c
Karthik Vinod <karthivinu111222@gmail.com>
2025-07-24 00:49:53 +0400

    notifications2334

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ const App = () => {
         serviceWorkerPath: "service-worker.js",
         serviceWorkerUpdaterPath: "service-worker-updater.js",
       }).then(() => {
-        OneSignal.Slidedown.promptPush();
+        OneSignal.showSlidedownPrompt();
       });
     }
   }, []);
